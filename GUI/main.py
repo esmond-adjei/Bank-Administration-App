802a572bbcb79927fd100aae2c80bcbb88b886f4
esmond-adjei <81225469+esmond-adjei@users.noreply.github.com>
2022-09-03 19:21:58 +0000

    modified:   GUI/main.py

--- GUI/main.py
+++ GUI/main.py
@@ -7,10 +7,10 @@ window_properties = {
     'root':root,
     'WIN_WIDTH':640,
     'WIN_HEIGHT':360,
-    'DEFAULT_FONT':'montserrat',
-    'DEFAULT_FONT_SIZE':10,
     'THEME_DARK':'#283A73',
     'THEME_LIGHT':'#ccc',
+    'DEFAULT_FONT':'montserrat',
+    'DEFAULT_FONT_SIZE':10,
 }
 
 # WINDOW SETUP
@@ -20,6 +20,7 @@ root.geometry(f"{window_properties['WIN_WIDTH']}x{window_properties['WIN_HEIGHT'
 root.configure(bg=window_properties['THEME_LIGHT'])
 root.resizable(False,False)
 
+root = window_properties['root']
 home_view(window_properties)
 
 root.mainloop()
